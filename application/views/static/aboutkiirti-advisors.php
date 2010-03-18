23b30db5687a439da778cf282208404b88c81696
Selvam Velmurugan <selvam@emoksha.org>
2010-03-18 02:06:20 +0000

    Adding Mike to advisors

--- application/views/static/aboutkiirti-advisors.php
+++ application/views/static/aboutkiirti-advisors.php
@@ -19,10 +19,15 @@ Kiirti is an initiative by <a href="http://www.emoksha.org">eMoksha</a> to promo
 	 <img src='/media/img/divider.gif'>
    </div>
    <div id="left-content">
-	<span class="title">Advisors</span><br>
+	<span class="title">Advisors</span><br><br>
         <span class="copy">
-<h3>Sunil Abraham</h3>
+<b>Sunil Abraham</b>
 <p>Sunil Abraham (Executive Director) is a Bangalore-based social entrepreneur and Free Software advocate. He founded <a href="http://www.mahiti.org/"><u>Mahiti</u></a> in 1998 which aims to reduce the cost and complexity of Information and Communication Technology for the Voluntary Sector by using Free Software. Today, Mahiti employs more than 50 engineers and Sunil continues to serve on the board. He was elected an <a href="http://www.ashoka.org/"><u>Ashoka</u></a> fellow in 1999 to 'explore the democratic potential of the Internet'. He was granted a <a href="http://www.sarai.net/"><u>Sarai</u></a> FLOSS fellowship in 2003. Between June 2004 and June 2007, Sunil also managed the <a href="http://www.iosn.net/"><u>International Open Source Network</u></a> a project of <a href="http://www.undp.org/"><u>United Nations Development Programme's</u></a> <a href="http://www.apdip.net/"><u>Asia-Pacific Development Information Programme</u></a> serving 42 countries in the Asia-Pacific region. Between September 2007 and June 2008, he managed <a href="http://www.enrap.org/"><u>ENRAP</u></a> an electronic network of <a href="http://www.ifad.org/"><u>International Fund for Agricultural Development</u></a> projects in the Asia-Pacific facilitated and co-funded by <a href="http://www.idrc.ca/"><u>International Development Research Centre</u></a>, Canada.</p>
+	</span>
+	<img src='/media/img/line580.gif'>
+        <span class="copy">
+<b>Mike Mathieu</b>
+<p>Mike Mathieu is the founder of <a href="http://www.frontseat.org">Front Seat</a>, a civic software company and incubator. Prior to Front Seat, Mike spent six years as founder and CEO of <a href="http://www.allstardirectories.com">All Star Directories</a>, an INC 500-listed publisher of online and career school directories. During the 1990's, Mike was General Manager of <a href="http://msn.com">MSN.com</a> and the MSN Search engine at Microsoft, and helped build Microsoft Excel and Word into the leading office applications. He is a partner and former lead partner in <a href="http://www.svpseattle.org">Social Venture Partners</a>, a leading U.S. venture philanthropy network and past Board President of the Technology Access Foundation. Mike holds a BA, Magna Cum Laude in the History of Science from <a href="http://www.harvard.edu">Harvard University</a>, and lives in Seattle with his wife and two kids. </p>
 	</span>
 	<img src='/media/img/line580.gif'>
     </div>
