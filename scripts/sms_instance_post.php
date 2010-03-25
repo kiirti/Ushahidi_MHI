4544f888d92d8b71d875b1d6c2af86f053eb83c0
Selvam Velmurugan <selvam@emoksha.org>
2010-03-25 18:57:47 +0000

    Fixed issue with isntance SMS routing using curl

--- scripts/sms_instance_post.php
+++ scripts/sms_instance_post.php
@@ -30,7 +30,7 @@ if (!$result){
 
 while($row = mysql_fetch_assoc($result)){
   print "\nMessage: ". $row['message']."\n";
-  $q2 = sprintf ("select SUBSTRING_INDEX(SUBSTRING_INDEX('%s',' ',2),' ',-1)", mysql_real_escape_string($row['message']));
+  $q2 = sprintf ("select SUBSTRING_INDEX('%s',' ',1)", mysql_real_escape_string($row['message']));
   $rcat1 = mysql_query($q2);
   $rcat = mysql_fetch_row($rcat1);
   print "Instance SMS ID: ". $rcat[0]."\n";
@@ -38,36 +38,31 @@ while($row = mysql_fetch_assoc($result)){
   $qSit = mysql_query($qS);
   $numResults = mysql_num_rows($qSit);
   if ($numResults > 0) {
-          $qSites = mysql_fetch_row($qSit);
-	  $url = "http://".$qSites[0].$mainSite;
-          $postData = "key=".urlencode($qSites['1'])."&s=".urlencode($row['message_from'])."&m=".urlencode($row['message']);
-	  $url = $url."?".$postData;
-	  print "URL: ".$url."\n";
-	  $ch = curl_init();
-curl_setopt($ch, CURLOPT_VERBOSE, 1);
-curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
-//curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
-//curl_setopt($ch, CURLOPT_DNS_USE_GLOBAL_CACHE, 0);
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_DNS_CACHE_TIMEOUT, 0);
-	  $http_result = curl_exec($ch);
+        $qSites = mysql_fetch_row($qSit);
+	$url = "http://".$qSites[0].$mainSite;
+        $postData = "key=".urlencode($qSites['1'])."&s=".urlencode($row['message_from'])."&m=".urlencode($row['message']);
+	$url = $url."?".$postData;
+	print "URL: ".$url."\n";
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
+	$http_result = curl_exec($ch);
 
-	  $error = curl_error($ch);
-	  $http_code = curl_getinfo($ch ,CURLINFO_HTTP_CODE);
-	  print "HTTP Code: ".$http_code."\n";
-	  curl_close($ch);
-usleep(250000);
-	  if ($error) {
+	$error = curl_error($ch);
+	$http_code = curl_getinfo($ch ,CURLINFO_HTTP_CODE);
+	print "HTTP Code: ".$http_code."\n";
+	curl_close($ch);
+	sleep(2);
+	if ($error) {
 		print "Error: $error\n";
-	  }
-	  else {
+	}
+	else {
   		$q3 = sprintf ("update message set message_detail = 'processed' where id = %s", mysql_real_escape_string($row['id']));
   		mysql_query($q3);
-	  }
+	}
    } else {
-   	  print "No instance associated with this ID: ".$rcat[0]."\n";
-          $q4 = sprintf ("update message set message_detail = 'error' where id = %s", mysql_real_escape_string($row['id']));
-          mysql_query($q4);
+   	print "No instance associated with this ID: ".$rcat[0]."\n";
+        $q4 = sprintf ("update message set message_detail = 'error' where id = %s", mysql_real_escape_string($row['id']));
+        mysql_query($q4);
    }
    mysql_free_result($qSit);
    mysql_free_result($rcat1); 
--- scripts/sms_routing.sh
+++ scripts/sms_routing.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
+
 # Script to run the upd_SMSID_In_SitesTable procedure to update InstanceSMS_ID field with the row ID
-mysql -h kiirtidb.c4iuunoxp2j3.us-east-1.rds.amazonaws.com -uemoksha -pem0ksh2 mhidb --skip-column-names -e "call upd_SMSID_In_SitesTable();" >> /root/kiirti/multiple/mog/Ushahidi_MHI/log/kiirti_SMSID_Creation.log 2>&1
-# Script to run the ins_Msg_Instance stored procedure
-# This SP checks the mhidb message table and inserts the rows present in the respective instance db
-# mysql -h kiirtidb.c4iuunoxp2j3.us-east-1.rds.amazonaws.com -uemoksha -pem0ksh2 mhidb --skip-column-names -e "call ins_Msg_Instance();" >> /root/kiirti/multiple/mog/Ushahidi_MHI/log/kiirti_SMS_insert_SP.log 2>&1
+mysql -h kiirtidb.c4iuunoxp2j3.us-east-1.rds.amazonaws.com -uemoksha -p mhidb --skip-column-names -e "call upd_SMSID_In_SitesTable();" >> /root/kiirti/multiple/mog/Ushahidi_MHI/log/kiirti_SMSID_Creation.log 2>&1
+
 # This script below will call the frontlinesms url using rows in the message table
 cd /root/kiirti/multiple/mog/Ushahidi_MHI && php scripts/sms_instance_post.php >> /root/kiirti/multiple/mog/Ushahidi_MHI/log/kiirti_sms_instance_post.log 2>&1
